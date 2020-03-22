37d10ef5357b8d3f519ab58ac16afd5df41e679e
Jaime Echevarria <jaime030690@gmail.com>
2020-03-22 18:30:57 -0400

    added form listener to filter table

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,15 +1,43 @@
 // from data.js
 var tableData = data;
 
-// Add table to document
-var tbody = d3.select("tbody");
+// function to add table data
+function createTable(elements) {
+    // Add table to document
+    var tbody = d3.select("tbody");
+    
+    // Clear table
+    tbody.html("");
 
-tableData.forEach((ufo_data) => {
-    var row = tbody.append("tr");
+    console.log("cleared table!");
+
+    elements.forEach((ufo_data) => {
+        var row = tbody.append("tr");
     
-    Object.entries(ufo_data).forEach(([key, value]) => {
+        Object.entries(ufo_data).forEach(([key, value]) => {
     
-        var cell = row.append("td");
-        cell.text(value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
+}
+
+// Form listener
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);    // check
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    console.log(filteredData);  // check
+
+    createTable(filteredData);
+
 });
+
+// Run function at least once to create default table
+createTable(tableData);
